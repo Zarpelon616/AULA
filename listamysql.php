8adfc777e70e4c9b13eca68d5b14798666d797c0
Zarpelon <100882885+Zarpelon616@users.noreply.github.com>
2023-07-02 17:12:03 -0300

    Create listamysql.php

---
+++ listamysql.php
@@ -0,0 +1,6 @@
+<?php
+define('DNS', 'mysql');     //DNS: Database Source Name
+define('SERVIDOR', 'localhost');
+define('USUARIO', 'root');
+define('SENHA', '');
+define('BANCODEDADOS', 'unoesc_php');
